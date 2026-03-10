aa59ce7ee6b1b48a4e3338d02e96b545c97bde88
Norinot <bencemark.bernath@gmail.com>
2026-03-10 22:54:25 +0100

    Update all imports, geomoetry.json

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -11,5 +11,8 @@ import '@kinetik/core/src/components/textarea/kp-textarea.ts';
 import '@kinetik/core/src/components/card/kp-card.ts';
 import '@kinetik/core/src/components/icon/kp-icon.ts';
 import '@kinetik/core/src/components/checkbox/kp-checkbox.ts';
+import '@kinetik/core/src/components/popover/kp-popover.ts';
+import '@kinetik/core/src/components/select/kp-select.ts';
+import '@kinetik/core/src/components/option/kp-option.ts';
 
 import './style.css';
